8bfcd812dff581524e4cb888b45515ec588b6467
nguyenvanpho <pho@nanohn.com>
2018-01-30 17:23:34 +0700

    fix scss, add file _variables.scss, responsive.scss, fix file js, html

--- js/carousel-customers.js
+++ js/carousel-customers.js
@@ -1,51 +1,46 @@
  $(document).ready(function() {
-    var owl = $('.select-carousel');
-      owl.owlCarousel({
-          padding: 10,
-          // nav: true,
-          loop: true,
-          responsive: {
-            0: {
-              items: 3,
-              autoplay:true,
-              autoplayTimeout:2000,
-            },
-            768: {
-              items: 5
-            },
-            1000: {
-              items: 5
-            }
+  let owl_select = $('.select-carousel');
+    owl_select.owlCarousel({
+        padding: 10,
+        // nav: true,
+        loop: true,
+        responsive: {
+          0: {
+            items: 3,
+            autoplay:true,
+            autoplayTimeout:2000,
+          },
+          768: {
+            items: 5,
+            loop:false,
+          },
+          1000: {
+            items: 5,
+            loop:false,
           }
-      })
-      $('.play').on('click', function() {
-          owl.trigger('play.owl.autoplay', [1000])
-      })
-      $('.stop').on('click', function() {
-          owl.trigger('stop.owl.autoplay')
-  })
+        }
+    })
+
+   let owl_customers = $('.our-customers-employees');
+    owl_customers.owlCarousel({
+      autoplay:true,
+      autoplayTimeout:2000,
+      padding: 10,
+      // nav: true,
+      loop: true,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 4
+        },
+        768: {
+          items: 6
+        },
+        992: {
+          items: 8
+        }
+      }
+    })
 })
 
  
- $(document).ready(function() {
-              var owl = $('.our-customers-employees');
-              owl.owlCarousel({
-                autoplay:true,
-                autoplayTimeout:2000,
-                padding: 10,
-                // nav: true,
-                loop: true,
-                autoplayHoverPause: true,
-                responsive: {
-                  0: {
-                    items: 4
-                  },
-                  768: {
-                    items: 6
-                  },
-                  992: {
-                    items: 8
-                  }
-                }
-              })
-            })
